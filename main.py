ad28eba858dd07f71720290365b251d976ba0f52
unknown <prohanter34@gmail.com>
2023-02-05 22:05:01 +0300

    one route app

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 from flask import Flask, render_template, request
-import psycopg2
+# import psycopg2
 
-@
+app = Flask(__name__)
+@app.route("/")
+def main():
+    return render_template("index.html")
