e50563f0acd3b1a41823a244f95905556f95e62b
unknown <prohanter34@gmail.com>
2023-02-05 22:51:07 +0300

    refactoring

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from flask import Flask, render_template, request
 # import psycopg2
 
 app = Flask(__name__)
-@app.route("/login")
-def main():
-    return render_template("index.html")
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    if auth(request.form["username"], request.form["password"]):
+    if request.method == "POST":
+        if auth(request.form["username"], request.form["password"]):
+            return render_template("me.html")
+        else:
+            return render_template("index.html")
+    elif request.method == "GET":
         return render_template("index.html")
-    else:
-        return render_template("me.html")
+
+
 
 def auth(login, password):
     if True:
