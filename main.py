24271bc5b729c5ceea83089276867cacdad3b64b
unknown <prohanter34@gmail.com>
2023-02-05 22:36:03 +0300

    add post method

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request
 # import psycopg2
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/login")
 def main():
+    return render_template("index.html")
+@app.route("/login", methods=["POST"])
+def login():
+    print(str(request.form["username"]))
+    print(str(request.form["password"]))
     return render_template("index.html")
